bc102c4d0cc20c8644fbfd01ac9feea1fb555e92
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-01-19 19:17:44 -0500

    Add Sinatra app authentication

--- app.rb
+++ app.rb
@@ -1,12 +1,24 @@
+require("sinatra")
 require_relative("lib/password_checker.rb")
 
 checker = PasswordChecker.new
 
-p checker.check_password("nizar@ironhack.com", "aZ0---")
-p checker.check_password("nizar@ironhack.com", "Z0--------")
-p checker.check_password("nizar@ironhack.com", "a0--------")
-p checker.check_password("nizar@ironhack.com", "aZ--------")
-p checker.check_password("nizar@ironhack.com", "aZ00000000")
-p checker.check_password("nizar@ironhack.com", "aZ0*nizar")
-p checker.check_password("nizar@ironhack.com", "aZ0*ironhack")
-p checker.check_password("nizar@ironhack.com", "aZ0--------")
+
+get "/" do
+  erb(:form)
+end
+
+post "/login" do
+  email = params[:email]
+  password = params[:password]
+
+  if checker.check_password(email, password) == "NO"
+    redirect to("/")
+  else
+    redirect to("/congratulations")
+  end
+end
+
+get "/congratulations" do
+  erb(:congratulations)
+end
